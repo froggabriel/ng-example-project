3f5abb07837ae7b8255dd9d985e52e1affcd24c4
gvidaurr <Gabriel.VidaurreRodriguez@windriver.com>
2019-07-19 11:16:30 -0600

    init

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Gabriel\'s Awesome Recipe', 'Just an awesome recipe', 'https://www.maangchi.com/wp-content/uploads/2008/03/dakweb-150x150.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
